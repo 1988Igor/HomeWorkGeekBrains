a96d5324dfcda95fc7b9c130257259fae498fd19
Igor <romanigor.conect@gmail.com>
2022-08-20 22:22:40 +0200

    Even numbers

---
+++ Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int result = numberA % 2;
+if (result == 0)
+{
+
+    Console.WriteLine("This number is Even");
+}
+else
+{
+
+    Console.WriteLine("The number is not Even");
+}
