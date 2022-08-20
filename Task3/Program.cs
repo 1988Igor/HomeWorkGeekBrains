db3f17f15f78936f8b8c8482efc895e30f3e7439
Igor <romanigor.conect@gmail.com>
2022-08-20 19:35:17 +0200

    Find the max number from 3 numbers

---
+++ Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int a,b,c;
+Console.WriteLine("Enter the firstnumber:");
+a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the secondnumber:");
+b = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter the thirdnumber:");
+c = int.Parse(Console.ReadLine());
+int max = 1;
+if (max<a) max=a;
+{
+
+}
+if (max<b) max=b;
+{
+
+}
+if (max<c) max=c;
+{
+
+}
+Console.Write("max = " + max);
