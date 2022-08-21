3b4cee4c860305304398caac127aca6000bb0700
Igor <romanigor.conect@gmail.com>
2022-08-22 00:17:46 +0200

    multiple or not multiple

---
+++ Task9/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Enter a number :");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter another number :");
+int n2 = int.Parse(Console.ReadLine());
+
+
+if (n1%n2==0)
+{
+    Console.WriteLine("The number " +n1+ " is multiple to number " + n2);
+}
+
+else 
+{
+    Console.WriteLine("The number " +n1+  " is not multiple to number " + n2 +  ". The Remainder is " + n1%n2 );
+}
+
+
+
+
