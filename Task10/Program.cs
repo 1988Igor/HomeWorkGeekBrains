97d5f154269daec3cec6893c3c2652040f351ec0
Igor <romanigor.conect@gmail.com>
2022-08-22 01:10:35 +0200

    find a third number

---
+++ Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Enter a three-digit number :");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 100 || n>999)
+{
+    Console.WriteLine("It was entered a not three-digit number! ");
+    Console.WriteLine("The third number of digit could not be find!");
+    Console.WriteLine("Please try again!");
+ 
+ }
+
+else
+{
+Console.WriteLine("The third number of digit is: " +  n %10);
+}
+
+
+
+
+    
+
