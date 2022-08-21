6102b7c036cd6f6d01b7077492c53c58542fdcbb
Igor <romanigor.conect@gmail.com>
2022-08-21 19:55:52 +0200

    max number from digit

---
+++ Task7/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter a number from [10,99]");
+int n = int.Parse(Console.ReadLine());
+int n1 = n/10;
+int n2 = n%10;
+int max = 0;
+if (n1>n2) max = n1;
+if (n1<n2) max = n2;
+Console.WriteLine($"The maximal number is {max}");
